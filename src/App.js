bc4a74ea0408302b65a81a8757f9cf4891078552
Roshan852k <roshany852k@gmail.com>
2024-11-24 18:37:03 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Login from "./Components/Login"; // Importing the Login component
+import About from "./Components/Dashboard"; // Importing the Login component
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "./App.css"; 
+
+// function App() {
+//   return (
+//     <div>
+//       <Login />
+//     </div>
+//   );
+// }
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* Add your navigation bar or links here if needed */}
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
